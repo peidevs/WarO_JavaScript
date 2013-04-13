d2abdc297adf19e04670c84ca3425dd11c404e86
evanepio <evan.porter@dotman.ca>
2013-04-13 17:36:23 -0300

    Removing unused kittyIndex and whitespace.

--- war-o.js
+++ war-o.js
@@ -1,16 +1,10 @@
-
 var numberOfPlayers = 3;
-var kittyIndex = numberOfPlayers;
-
 var maxValue = 60;
-
 
 function shuffle(o){ //v1.0
     for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 }
-
-
 
 function createPlayer() {
 	return { 
