e652f2c55e2531a6f808d9e7c1b0137ad997987b
evanepio <evan.porter@dotman.ca>
2013-05-05 13:59:22 -0300

    Adding unit tests for splitDeck function.

--- war-o-tests.js
+++ war-o-tests.js
@@ -52,4 +52,29 @@ test( "WARO.splitDeck - 5-element array split into length 1 arrays gives 5 split
     equal(splits.length, 5, "Passed");
 });
 
+test( "WARO.splitDeck - 5-element array split into length 5 arrays gives 1 split", function () {
+    var deck = [1,2,3,4,5];
 
+    //test
+    var splits = WARO.splitDeck(deck, 5);
+
+    equal(splits.length, 1, "Passed");
+});
+
+test( "WARO.splitDeck - 6-element array split into length 2 arrays gives first split of 3 elements", function () {
+    var deck = [1, 2, 3, 4, 5, 6];
+
+    //test
+    var firstSplit = WARO.splitDeck(deck, 2)[0];
+
+    equal(firstSplit.length, 2, "Passed");
+});
+
+test( "WARO.splitDeck - 6-element array split into length 4 arrays gives first split of 4 elements", function () {
+    var deck = [1, 2, 3, 4, 5, 6];
+
+    //test
+    var firstSplit = WARO.splitDeck(deck, 4)[0];
+
+    equal(firstSplit.length, 4, "Passed");
+});
